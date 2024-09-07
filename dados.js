1c966bba6fce5b6be656f2ae7870989215a35c8a
ELIANE POLIDORO BERTI <107213201+ELIANEBERTI@users.noreply.github.com>
2024-09-06 23:09:12 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,95 @@
+let dados = [
+    {
+        titulo: "TOURO",
+        descricao: "Signo de Touro - o Teimoso !(21/04 a 20/05)Os taurinos são esforçados e práticos!Além disso, são regidos pelo planeta Vênus, e por isso admiram muito a beleza das coisas.Características positivas dos taurinos: cuidadoso com tudo e com todos, é paciente e produtivo. Desse modo, são pessoas em que se podem confiar e costumam ser muito fiéis;Características negativas dos taurinos: a teimosia é característica nata de um taurino. Visto que são muito persistentes e obstinados, querem cuidar de tudo e podem se mostrar centralizadores, inflexíveis e materialistas. DICAS COMO PROGRAMADOR: Um bom taurino preza pelo bom ambiente para trabalhar: uma cadeira confortável, um bom equipamento. Como apreciam a beleza das coisa, são ótimos para criar site com visual agradável e de extremo bom gosto. Front end de primeira! Por outro lado, o seu lado teimoso, persistente também o qualifica a tornar-se um eximio Backend,pois enquanto não resolve um enigma, Touro não sossega...Só vai parar para fazer um lanchinho,,, porque afinal de contas Toura ama um bom cardápio!",
+        link: "https://astrosigno.com/personalidade-de-cada-signo/",
+        tags: ""
+   },
+    {
+        titulo: "GEMEOS",
+        descricao:"Signo de Gêmeos (21/05 a 20/06) - O Tagarela ! Os geminianos são extrovertidos e amigões!Ainda, são regidos pelo planeta Mercúrio, o que traz grande capacidade de adaptação e comunicação a eles. Tem o dom de fazer a leitura do que o outro quer ouvir e são muito bons com palavras, argumentos e encenações.Características positivas dos geminianos: animados e comunicativos, eles tem a mente aberta e são muito espertos;Características negativas dos geminianos: podem oscilar muito, tonando-se indecisos, inconstantes, ansiosos e ao mesmo tempo evasivos. As vezes parece que tem 10 pessoas diferentes dentro de um geminiano;  DICAS COMO PROGRAMADOR:Você não é muito disciplinado e não curte muito resolver problemas de lógica, portanto Back End não é a sua praia. Mas você sabe o que o cliente gosta e sabe interpretar muito bem, além de ser muito criativo.. Então O Front End, com sua criatividade e facilidade de expressão, seria a melhor pedida!  Provavelmente suas criações irão impressionar a todos! Invista em seu potencial!",
+        link: "https://astrosigno.com/personalidade-de-cada-signo/",
+        tags: "GÊMEOS"
+    },
+    {
+        titulo: "CANCER",
+        descricao: "Signo de Câncer (21/06 a 21/07) - O Queridinho da Galera!A principal marca dos cancerianos é a sensibilidade e o carinho.Inclusive, são regidos pela Lua, que é diretamente ligada ao nosso emocional.Características positivas dos cancerianos: são dedicados e gentis, sempre ouvindo ou cuidando de alguém por perceberam suas emoções e problemas. Características negativas dos cancerianos: a carência e o mau humor podem surgir a qualquer momento nos cancerianos, que podem se tornar ciumentos ou se vitimizar.  DICAS COMO PROGRAMADOR: Você é um excelente lider para conduzir um Squad,resolver conflitos, melhorar ambiente de trabalho.  É o cara que vai organizar a festa de final de ano, os aniversários e todas as festas. Cuidado ao assumir prazos no seu trabalho... Organize-se para poder entregar os resultados. Sua dedicação pode levá-lo a desempenhar qualquer atividade, tanto no front, quanto no back.,",
+        link: "https://astrosigno.com/personalidade-de-cada-signo/",
+        tags: "CÂNCER, CARANGUEJO"
+    },
+    {
+        titulo: "ARIES",
+        descricao: "Signo Áries (21/03 a 20/04) O Nervosinho! Os arianos costumam ser individualistas e espontâneos! Características positivas dos arianos: se coloca em primeiro lugar mas sem esquecer dos demais à sua volta! Motiva as pessoas e é prestativo além de perseverante e apaixonado;   Características negativas dos arianos: sua agitação faz com que o ariano normalmente seja inquieto, impulsivo ou inconsequente. DICAS COMO PROGRAMADOR:  Não queira tudo pra ontem. Respire, relaxe, converse com outros colegas. Com certeza seu programa será um dos melhores e que trará resultados imediatos! Vai fundo.. com uma pitada de paciencia você chega lá ! Voce é o Cara! ",
+        link: "https://astrosigno.com/personalidade-de-cada-signo/",
+        tags: "CARNEIRO"
+    },
+    {
+        titulo: "LEAO",
+        descricao: "Signo de Leão (22/07 a 22/08) - O Forte ! s leoninos são vaidosos e confiantes.Sem dúvida, por serem regidos pelo Sol, o nosso astro rei! Características positivas dos leoninos: inteligentes e corajosos, são líderes natos. Por outro lado, são expressivos e determinados: quando colocam algo na mente, pode ter certeza que vão até o fim para conquistá-la;Características negativas dos leoninos: têm a personalidade muito forte, podendo se tornar ambiciosos, mandões ou convencidos demais;DICAS COMO PROGRAMADOR:  Sua auto estima e determinação lhe darão a energia necessária para entregar qualquer trabalho que lhe for solicitado.. Habilidoso para relacionar-se com a galera, costuma obter ajudas importantes. Se não sei fazer, sou amigo de quem sabe... Este é o Leonino... sempre bem relacionado e aceitando todos os desafios que surgirem em seu caminho.. Como é muito vaidoso, o Front End é seu lugar, pois poderá desfrutar de muitos elogios pelos trabalhos criados... Gosta de resultados rápidos. Ficaria entediado trabalhando nos bastidores do BackEnd.",
+        link: "https://astrosigno.com/personalidade-de-cada-signo/",
+        tags: "LEÃO"
+    },
+    {
+        titulo: "VIRGEM",
+        descricao: "Signo de Virgem (23/08 a 22/09)- O Certinho ! Organizados e analíticos: esses são os virginianos.Características positivas dos virginianos: observação e eficiência são marcas do virginiano, que é bastante atencioso e sincero com os demais e está sempre resolvendo um problema: seja seu ou dos outros.Características negativas dos virginianos: ser sistemático torna o virginiano cheio de manias, muito crítico e desconfiado. Também pode torná-los ansiosos e perfeccionistas demais. DICAS COMO PROGRAMADOR :  Se tem um signo que combina perfeitamente com o Back End... é o Virginiano, pois é trabalhador incansável, adora lógica e não gosta de ficar nos holofotes.. Mas fica magoado quando seu trabalho não é reconhecido!  Gosta de entregar sempre o melhor.. tudo bem feito.. minuciosamente pensado, criado e revisado... Acho até que quem criou este conceito de back end deve ter sido um virginiano...  Quanto mais virginianos você tiver na sua equipe de DEV´s mais feliz você será.. Entregam muiiittto!",
+        link: "https://astrosigno.com/personalidade-de-cada-signo/",
+        tags: "VIRGEN, VIRGO"
+    },
+    {
+        titulo: "LIBRA",
+        descricao: "Signo de Libra (23/09 a 22/10) O indeciso !  Clichê dizer, mas o equilíbrio e a justiça definem os librianos.Características positivas dos librianos: extremamente diplomáticos, são sinceros e buscam ponderar sempre para chegar no melhor resultado. Para isso contam com sua simpatia, boa comunicação e tranquilidade.Características negativas dos librianos: querer atingir o equilíbrio sempre torna o libriano indeciso, controlador e com muita mania de organização. Também têm dificuldade de dizer não em algumas situações. DICAS COMO PROGRAMADOR: Libriano possui excelente capacidade de compilar informações e transformá-la em comunicação.. São muito hábeis e caprichosos em suas tarefas. São as pessoas mais fáceis de relacionamento do zoodíaco... E festeiro, alegre.Adora criar eventos. o Problema do Libriano será sempre a indecisão... Que cor eu escolho para meu background? vai mudar de ideia umas 10 vezes com certeza até a versão final... E vai entregar um trabalho perfeito ! Criado com muito carinho e dedicação ! Deixa ele no Front-end... vai se realizar! ",
+        link: "https://astrosigno.com/personalidade-de-cada-signo/",
+        tags: "BALANCA,BALANÇA"
+    },
+    {
+        titulo: "ESCORPIAO",
+        descricao: "Signo de Escorpião (23/10 a 21/11) - O determinado ! Atraente e intenso: esse é o escorpiano.Características positivas dos escorpianos: de uma auto-confiança invejável (a não ser pelos leoninos…rs), têm muita intuição e um instinto protetor maravilhoso. São dedicados e apaixonados.Características negativas dos escorpianos: sua boa memória, ciúmes e às vezes obsessão fazem com que o nunca esqueçam do que lhe fizeram, se tornando vingativos e rancorosos. DICAS COMO PROGRAMADOR: Como é bom trabalhar com um Escorpiano ao seu lado.. Ele nunca deixará você desistir..Sua persistencia o faz um grande DEV.. O BackEND é sua praia! Gosta de desafios, quanto mais complexo melhor... Vai fundo em todas as questões e possui uma intuição impressionante... Sabe aqueles DEV´s que farejam o erro... este é o Escorpiano... Se fizer dupla com um virginiano então.... Bill Gates que se cuide...",
+        link: "https://astrosigno.com/personalidade-de-cada-signo/",
+        tags: "ESCORPIÃO"
+    },
+    {
+        titulo: "SAGITARIO",
+        descricao: "Signo de Sagitário (22/11 a 21/12)- O Sincerão !Independência e liberdade marcam os sagitarianos.Características positivas dos sagitarianos: são talentosos, inteligentes e justos. São pessoas estimulantes, que gostam de se divertir e têm bastante espontaneidade e vitalidade. Características negativas dos sagitarianos: podem aparentar arrogância por saberem demais, rígidos com si mesmos e os outros, um pouco impacientes e sarcásticos. DICAS COMO PROGRAMADOR :  São pessoas de fácil relacionamento em todos os grupos, dotadas de um poder agregador. Conseguem compilar dados e sintetizar as próximas ações.   As vezes cansam de rotinas.. não apreciam tarefas repetitivas.. Por isso são indicadas para projetos de curta duração, pois sua energia é alta e Sagitário funciona como uma pilha.. no inicio é super potente.. mas no final vai reduzindo o rítmo... O backend não lhe é indicado... Já no Front,,,. ele se destacará com tanto brilho que logo após entregar um bom trabalho, já estará recarregado para um novo desafio",
+        link: "https://astrosigno.com/personalidade-de-cada-signo/",
+        tags: "SAGITÁRIO"
+    },
+    {
+        titulo: "CAPRICORNIO",
+        descricao: "Signo de Capricórnio (22/12 a 21/01)  - o Responsável ! A responsabilidade é o ponto forte do capricorniano. Características positivas dos capricornianos: são persistentes e determinados. Sua organização e ambição natas fazem com que sejam muito eficientes e trabalhadores. Características negativas dos capricornianos: muito certinhos, podem se tornar rígidos e orgulhosos demais. Por serem muito práticos, também podem se tornar frios e pessimistas. DICAS DE PROGRAMADOR:Assim como os virginianos, os Capricornianos nasceram para o BackEnd..  São rigorosos com prazos, métodos e entregam tudo que se comprometem.  Não gostam de interferencia no seu trabalho, porém, sentem necessidade de reportar a um superior todas as evoluções que realizou e gosta de ser reconhecido por esta dedicação.  Para capricornio,  missão dada é missão cumprida !",
+        link: "https://astrosigno.com/personalidade-de-cada-signo/",
+        tags: "capricórnio,cabra"
+    },
+    {
+        titulo: "AQUARIO",
+        descricao: "Signo de Aquário (22/01 a 19/02) O Criativo > Os aquarianos são amantes dos desafios e da liberdade! Características positivas dos aquarianos: idealista e inventivo, o aquariano costuma ser tranquilo e justo, ao mesmo tempo que gosta de transformar e não se acomodar nas situações.Características negativas dos aquarianos: por ser individualista e desapegado, distante, rebelde e imprevisível.O signo de Aquário talvez seja o signo mais individualista do zodíaco.  DICA COMO PROGRAMADOR : Sabe aquela frase famosa Silencio... GÊNIO CRIANDO! Pode ter certeza que é um aquariano....   Ele é muito criativo...mas não gosta muito de dividir esta criatividade com os colegas enquanto esta em processo...  Depois de criado, ele tem um pouco de dificuldade em vender sua ideia...Então, as vezes, excelentes ideias ficam engavetadas porque o aquariano não recebeu a atenção necessária.. Conselho > Compartilhe, trabalhe em equipe, busque aliados..Você é muito bom no que faz.. mas não se isole numa ilha.. Ainda mais você que é tão preocupado com ações que impactem positivamente toda sociedade..Acredita e se joga !",      
+        link: "https://astrosigno.com/personalidade-de-cada-signo/",
+        tags: "AQUÁRIO"
+    },
+    {
+        titulo: "PEIXES",
+        descricao: "Signo de Peixes (20/02 a 20/03) o Sonhador ! Emocionais e espiritualizados, esses são os piscianos.Características positivas dos piscianos: ouvintes natos, são confiáveis, intuitivos, prestativos e preocupados com os demais. São adaptáveis e simpáticos, os tornando bem sociáveis. Características negativas dos piscianos: a sensibilidade aflorada pode os tornar emocionais demais, tendendo para a depressão, os tornando muito temperamentais e rancorosos. São avoados, escapistas da realidade e por confiarem muito nos outros também costumam ser indecisos e inocentes.DICAS COMO PROGRAMADOR>   Mantenha o foco em suas atividades, cuide dos prazos e revise todos os detalhes.. Você provavelmente é o queridinho da turma e todos querem conviver contigo... Para se destacar no ramo da Programação, precisará de muita persistencia e determinação.  Se eu fosse você me especializava em FrontEnd pois você possui muita empatia com o Cliente e vai entregar um trabalho criado com muita paixão.",
+        link: "https://astrosigno.com/personalidade-de-cada-signo/",
+        tags: "PEIXE"
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
